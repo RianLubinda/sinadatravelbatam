061fa9e1b1909fd9089e6e068ed7ebd46c2e1b1b
RianLubinda <rianlubinda.work@gmail.com>
2025-06-07 11:05:27 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,36 +1,36 @@
-// import { auth, provider } from "./firebase-config.js";
-// import {
-//   signInWithPopup,
-//   signOut,
-//   onAuthStateChanged,
-// } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth, provider } from "./firebase-config.js";
+import {
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// const loginBtn = document.getElementById("loginBtn");
-// const logoutBtn = document.getElementById("logoutBtn");
-// const userInfo = document.getElementById("userInfo");
+const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const userInfo = document.getElementById("userInfo");
 
-// // Login
-// loginBtn.addEventListener("click", () => {
-//   signInWithPopup(auth, provider).catch((error) => {
-//     console.error("Login gagal:", error.message);
-//     alert("Login gagal. Silakan coba lagi.");
-//   });
-// });
+// Login
+loginBtn.addEventListener("click", () => {
+  signInWithPopup(auth, provider).catch((error) => {
+    console.error("Login gagal:", error.message);
+    alert("Login gagal. Silakan coba lagi.");
+  });
+});
 
-// // Logout
-// logoutBtn.addEventListener("click", () => {
-//   signOut(auth);
-// });
+// Logout
+logoutBtn.addEventListener("click", () => {
+  signOut(auth);
+});
 
-// // Update UI login/logout
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     loginBtn.style.display = "none";
-//     logoutBtn.style.display = "inline-block";
-//     userInfo.textContent = `Logged in as ${user.displayName || user.email}`;
-//   } else {
-//     loginBtn.style.display = "inline-block";
-//     logoutBtn.style.display = "none";
-//     userInfo.textContent = "";
-//   }
-// });
+// Update UI login/logout
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
+    userInfo.textContent = `Logged in as ${user.displayName || user.email}`;
+  } else {
+    loginBtn.style.display = "inline-block";
+    logoutBtn.style.display = "none";
+    userInfo.textContent = "";
+  }
+});
